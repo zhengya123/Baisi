8fee9c77ef892bf6cdfa11637dafe0ead4f913b6
zhengya123 <919458797@qq.com>
2017-03-30 09:26:14 +0800

    Initial Commit

---
+++ Baisi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baisi
+//
+//  Created by dqong on 2017/3/30.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Baisi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Baisi
+//
+//  Created by dqong on 2017/3/30.
+//  Copyright © 2017年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
